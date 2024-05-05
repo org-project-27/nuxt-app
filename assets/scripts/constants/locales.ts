43b3132f8e6587554b598b1277599c1212d593e0
Vugar Safarzada <vuqarsfrzad@gmail.com>
2024-05-05 19:59:52 +0400

    Language switcher build bug fixed

--- assets/scripts/constants/locales.ts
+++ assets/scripts/constants/locales.ts
@@ -3,26 +3,23 @@ import en from "assets/locales/en";
 import az from "assets/locales/az";
 import ru from "assets/locales/ru";
 
-import azFlag from "@/assets/images/icons/flags/az-AZ.svg";
-import enFlag from "@/assets/images/icons/flags/en-US.svg";
-import ruFlag from "@/assets/images/icons/flags/ru-RU.svg";
 export let availableLocales: AvailableLocals = [
     {
         messages: az,
         label: 'az',
         iso: 'az-AZ',
-        icon: azFlag
+        icon: '/flags/az.svg'
     },
     {
         messages: en,
         label: 'en',
         iso: 'en-US',
-        icon: enFlag
+        icon: '/flags/en.svg'
     },
     {
         messages: ru,
         label: 'ru',
         iso: 'ru-RU',
-        icon: ruFlag
+        icon: '/flags/ru.svg'
     },
 ];
