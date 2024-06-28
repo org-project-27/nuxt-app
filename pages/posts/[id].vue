3a1d30ae697b4473e1b857bbccf62753fd92fdc0
Mehdi Safarzade <seferzademehdi@gmail.com>
2024-06-29 00:10:15 +0400

    Upload service update

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,19 +1,40 @@
 <template>
-  <div>
-    <input type="file" @change="uploadFile" multiple/>
+  <div class="uploader-part">
+    <label for="upload" class="btn">Upload File </label>
+    <input id="upload" type="file" @change="uploadFile" multiple />
     <div v-if="uploadResult">
       <p>File uploaded successfully:</p>
       <a :href="uploadResult.url" target="_blank">{{ uploadResult.name }}</a>
       <img :src="uploadResult.url" :alt="uploadResult.name" :width="dimensions?.width" :height="dimensions?.height">
     </div>
   </div>
+  <div class="uploader-part">
+    <label for="update" class="btn">Update File </label>
+    <input id="update" type="file" @change="updateFile" multiple />
+    <input type="text" placeholder="File id" name="id" id="updateFileId">
+    <div v-if="updateResult">
+      <p>File updateed successfully:</p>
+      <a :href="updateResult.url" target="_blank">{{ updateResult.name }}</a>
+      <img :src="updateResult.url" :alt="updateResult.name" :width="dimensions?.width" :height="dimensions?.height">
+    </div>
+  </div>
+  <div class="uploader-part">
+
+    <input type="text" placeholder="File id" name="id" id="deleteFileId">
+    <input id="delete" type="button" @click="deleteFile" value="Delete">
+    <div v-if="deleteResult">
+      <p>File deleted successfully:</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import {uploadService} from "~/services/upload";
+import {uploadService, updateService, deleteService} from "~/services/upload";
 
 const uploadResult = ref(null)
+const updateResult = ref(null)
+const deleteResult = ref(null)
 const dimensions = ref(null)
 
 const uploadFile = async (event) => {
@@ -24,6 +45,28 @@ const uploadFile = async (event) => {
     dimensions.value = { width, height }
   } catch (error) {
     console.error('Error uploading file:', error)
+  }
+}
+
+const updateFile = async (event) => {
+  try {
+    const fileId = document.getElementById('updateFileId').value
+    const response = await updateService(event, fileId);
+    updateResult.value = response[0]
+    const { width, height } = await getDimensions(response[0].url)
+    dimensions.value = { width, height }
+  } catch (error) {
+    console.error('Error updating file:', error)
+  }
+}
+
+const deleteFile = async () => {
+  try {
+    const fileId = document.getElementById('deleteFileId').value
+    const response = await deleteService(fileId);
+    deleteResult.value = response
+  } catch (error) {
+    console.error('Error deleting file:', error)
   }
 }
 
@@ -40,3 +83,8 @@ const getDimensions = async (url) => {
   })
 }
 </script>
+<style>
+  .uploader-part {
+    margin: 20px;
+  }
+</style>
