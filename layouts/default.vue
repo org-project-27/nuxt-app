289b5a78cf8636e6f5626a8499edac6d34fd0943
Vugar Safarzada <50525576+vugarsafarzada@users.noreply.github.com>
2024-07-11 18:20:56 +0400

    Changing font family

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,86 +1,97 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import availableAppRoutes from "~/constants/availableAppRoutes.js";
+import { useDeviceDetector } from '~/composables/useWindowSize';
+
+const { deviceType } = useDeviceDetector();
+const readyForView = computed(() => deviceType.value !== undefined && deviceType.value !== null);
+
+const deviceTypeSafe = computed(() => deviceType.value || 'unknown');
+</script>
+
 <template>
-  <div>
+  <loading-page-component v-if="!readyForView"/>
+  <div :id="`default-layout-${deviceTypeSafe}`"
+       v-if="readyForView"
+       class="container flex-column-start-center">
+    <header class="flex-row-between-center">
+      <div class="left-side">
+        <logo-component/>
+      </div>
+      <div class="middle">
+        search
+      </div>
+      <div class="right-side">
+        <nuxt-link :to="availableAppRoutes.get_started">
+          Login
+        </nuxt-link>
+        <nuxt-link :to="availableAppRoutes.account">
+          Signup
+        </nuxt-link>
+      </div>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
   </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-import { useAuthStore } from '~/stores/user/auth';
-import LoadingPageComponent from '~/components/LoadingPageComponent.vue';
-import HeaderComponent from "~/components/layouts/default/HeaderComponent.vue";
 
-export default defineComponent({
-  name: "DefaultLayout",
-  components: { LoadingPageComponent, HeaderComponent },
-  computed: {
-    authProgressIsLoading(){
-      return useAuthStore().authProgressIsLoading;
-    },
-    currentPath(){
-      return this.$route
-    },
-    renderKey(){
-      return useConfigsStore().renderKey;
-    }
-  },
-  data(){
-    return {
-      waitingForAuthProgress: null,
-    }
-  },
-  mounted(){
-    this.waitingForAuthProgress = this.authProgressIsLoading;
-  },
-  watch: {
-    authProgressIsLoading(val){
-      this.waitingForAuthProgress = val;
-    }
+<style scoped lang="scss">
+// Global:
+.container {
+  width: 100vw;
+  & > *{
+
   }
-});
-</script>
+  header {
 
-<style lang="scss">
-@include for-size($tablet-size, 100vw) {
-  $layout-width: 100%;
-  #default-layout.container{
-    gap: $default-layout-section-gap;
-    background-color: $main_background_color;
-    main{
-      border-radius: 1rem;
-      overflow: hidden;
-      min-height: 70vh;
-    }
-    footer{
-      min-height: 30rem;
+  }
+}
+
+// Mobile size:
+@include for-size($small-mobile-size, $tablet-size) {
+  #default-layout-mobile.container {
+    & > *{
+      background-color: lime !important;
       width: 100%;
+    }
+    header {
+      background-color: $header-background-color;
+      height: $default-layout-header-height;
       box-shadow: $box_shadow_1;
-      background-color: $footer_background_color;
-      color: $footer_font_color;
     }
   }
 }
 
-@include for-size($small-mobile-size, $tablet-size) {
-  $layout-width: 100%;
-  #default-layout.container{
-    height: 199px !important;
-    display: flex;
-    align-items: center;
-    gap: $default-layout-section-gap;
-    margin-top: $default-layout-header-height-mobile;
-    main{
-      margin-top: calc($default-layout-header-height-mobile / 2.5);
-      border-radius: 1rem;
-      overflow: hidden;
-      min-height: 70vh;
-    }
-    footer{
-      min-height: 30rem;
+// Tablet size:
+@include for-size($tablet-size) {
+  #default-layout-tablet.container {
+    & > *{
+      background-color: blue !important;
       width: 100%;
+    }
+    header {
+      background-color: $header-background-color;
+      height: $default-layout-header-height;
       box-shadow: $box_shadow_1;
-      background-color: $footer_background_color;
-      color: $footer_font_color;
     }
   }
+}
+
+// Desktop size:
+@include for-size($tablet-size, 100vw) {
+  #default-layout-desktop.container {
+    & > *{
+      background-color: red !important;
+      width: 80%;
+    }
+    header {
+      background-color: $header-background-color;
+      height: $default-layout-header-height;
+      box-shadow: $box_shadow_1;
+    }
+  }
+
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,84 +1,17 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+  name: "Home"
+})
+</script>
+
 <template>
     <div class="parent flex-row-center">
-        <NuxtLink
-            v-for="post in posts"
-            :id="'post-'+post.id"
-            :key="post.id"
-            :to="'/posts/'+post.place_id"
-            class="child cannot-select flex-column-center default-container">
-          {{post.name}}
-          <br>
-          <b>[{{post.Brands.name}}]</b>
-        </NuxtLink>
+      index
     </div>
 </template>
 
 <style lang="scss" scoped>
-
-
-@include for-size($tablet-size, 100vw) {
-  .parent{
-    display: flex;
-    flex-wrap: wrap;
-    gap: calc($default-layout-section-gap / 3);
-    .child{
-      box-shadow: $box_shadow_2;
-      border-radius: 10px;
-      width: 235px ;
-      height: 235px;
-      transition-duration: $default-transition-duration;
-      white-space: normal;
-      text-align: center;
-      overflow: hidden;
-      img{
-        width: 100%;
-        height: 100%;
-        opacity: .75;
-        &:hover{
-          cursor: pointer;
-          box-shadow: $box_shadow_0;
-          opacity: 1;
-        }
-      }
-      &:hover{
-        box-shadow: $box_shadow_0;
-      }
-    }
-  }
-}
-
-@include for-size($small-mobile-size, $tablet-size) {
-  .parent{
-    display: flex;
-    flex-wrap: wrap;
-    gap: calc($default-layout-section-gap / 3);
-    .child{
-      box-shadow: $box_shadow_1;
-      border-radius: 10px;
-      width: calc(100vw/2.5);
-      height: calc(100vw/2.5);
-      transition-duration: $default-transition-duration;
-      white-space: normal;
-      text-align: center;
-      overflow: hidden;
-      img{
-        width: 100%;
-        height: 100%;
-        opacity: .75;
-        &:hover{
-          cursor: pointer;
-          box-shadow: $box_shadow_0;
-          opacity: 1;
-        }
-      }
-      &:hover{
-        box-shadow: $box_shadow_0;
-      }
-    }
-  }
+* {
 }
 </style>
-<script setup lang="ts">
-let {getPosts} = usePostsStore();
-let posts = (await getPosts(200)).data.data.places.reverse();
-</script>
